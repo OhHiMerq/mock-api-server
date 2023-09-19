51ad3748e383da4e872e3ed6b87696ab6e8cb11f
OhHiMerq <dimailigv@gmail.com>
2023-09-19 15:48:25 +0800

    read delivery body

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/complete-delivery/", (req, res) => {
-  const { deliveryId, completedAt } = req.body;
-  const message = `Delivery ${deliveryId} is set to complete at ${completedAt}`;
+  const { deliveryId, completedAt } = req.body.delivery;
+  const message = `[${new Date()}] Delivery ${deliveryId} is set to complete at ${completedAt}`;
   console.log(message);
   res.json({ data: { input: req.body, message } });
 });
