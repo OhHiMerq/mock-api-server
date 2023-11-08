60e97b63d51c978130f06304ddd722892c89caf4
OhHiMerq <dimailigv@gmail.com>
2023-11-08 22:23:53 +0800

    update endpoint

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.get("/", (req, res) => {
   res.send("Mock API Server");
 });
 
-app.post("/complete-delivery/", (req, res) => {
-  const { deliveryId, completedAt } = req.body.delivery;
-  const message = `[${new Date()}] Delivery ${deliveryId} is set to complete at ${completedAt}`;
+app.post("/process-delivery/", (req, res) => {
+  const { deliveryId, processedAt, deliveryStatus } = req.body.delivery;
+  const message = `[${new Date()}] Delivery ${deliveryId} is set to ${deliveryStatus} at ${processedAt}`;
   console.log(message);
   res.json({ data: { input: req.body, message } });
 });
