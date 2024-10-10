54155364daf474045a1a4d0d64e7b8ec71c3ba16
OhHiMerq <dimailigv@gmail.com>
2024-10-10 23:17:24 +0800

    feat: introduce tokens and sap endpoint

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.post("/process-delivery/", (req, res) => {
   res.json({ data: { input: req.body, message } });
 });
 
+app.get("/token", (req, res) => {
+  console.log("TOKEN RECEIVED", req.body, req.headers);
+  res.header("x-csrf-token", "fetch");
+  res.header("set-cookie", "sap-cookie");
+  res.json({ data: { token: "token-sample", cookie: "cookie-sample" } });
+});
+
+app.post("/collection-sap", (req, res) => {
+  console.log("COLLECTION SAP RECEIVED", req.body, req.headers);
+  res.json({ data: { status: "SUCCESS!!" } });
+});
+
 app.listen(4000, () => {
   console.log("listening to port 4000");
 });
